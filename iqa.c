db9b910ba8cc1b5198d052d092d1a678287bf899
jefynho1213 <jefynho1213@hotmail.com>
2016-05-07 23:07:03 -0300

    Update iqa.c

--- iqa.c
+++ iqa.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 
 	int main(){
 
 		double param[8], peso[9] = {0.17, 0.15, 0.12, 0.10, 0.10, 0.10, 0.10, 0.08, 0.08} , teste[8]; 
-		 //oxigenio 0, coliformes 1, potencial 2, demanda 3, temperatura 4, nitrogenio 5, fosforo 6, turbidez 7, r total 8;
+		 //oxigenio 0, coliformes 1, potencial 2, demanda 3, temperatura 4, nitrogenio 5, fosforo 6, turbidez 7, rtotal 8;
 		int i;
 
 		for(i=0; i<=8; i++){
 			scanf("%lf", &param[i]);
 
 			}
+		
 
+	}
+	
+ 	void o2(void){
+ 	
+ 		double q9;
+ 	
+ 		q9=100,8*(pow(10, pow(param[0-160], 2)/-3745));
+ 	
+ 	}
+	void cf(void){
+		double q1;
+	
+		q1 = -98.03+(36.45*log10(param[1]))+(3,138*pow(log10(param[1]), 2))+(0.06776*pow(log10(param[1]), 3));
+		
+		
 	}
