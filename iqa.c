7c624320c64fb692897c7069793946790b5038d4
m4theussouza <m4theussouza@gmail.com>
2016-06-03 16:01:03 -0300

    Add files via upload

--- iqa.c
+++ iqa.c
@@ -5,7 +5,7 @@
 	int main()
 	{
 		
-		double o2, cf, ph, dbo, ft, nt, tu, vt, st, qo2, qcf, qph, qdbo, qft, qnt, qtu, qvt, qst;
+		double o2, cf, ph, dbo, ft, nt, tu, dt, st, qo2, qcf, qph, qdbo, qft, qnt, qtu, qst, qdt, media;
 		/*
 		o2 = percentual de oxigenio
 		cf = colifornes fecais
@@ -14,9 +14,31 @@
 		ft = fosforo total
 		nt = nitrogenio total
 		tu = turbidez
-		deltat = variacao de temperatura
+		dt = variacao de temperatura
 		st = solidos totais
 		*/
+
+		scanf("%lf", &o2);
+		scanf("%lf", &cf);
+		scanf("%lf", &ph);
+		scanf("%lf", &dbo);
+		scanf("%lf", &ft);
+		scanf("%lf", &nt);
+		scanf("%lf", &tu);
+		scanf("%lf", &dt);
+		scanf("%lf", &st);
+
+		qo2 = pow(e_o2(o2), peso_o2);
+		qcf = pow(e_cf(cf), peso_cf);
+		qph = pow(e_ph(ph), peso_ph);
+		qdbo = pow(e_dbo(dbo), peso_dbo);
+		qvt = pow(e_dt(dt), peso_dt);
+		qnt = pow(e_nt(nt), peso_nt);
+		qft = pow(e_ft(ft), peso_ft);
+		qtu = pow(e_tu(tu), peso_tu);
+		qdt = powe_st(st), peso_st);
+
+		media = qo2 * qcf * qph *; 
 
 
 
